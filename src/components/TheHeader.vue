664e46296ec0a710f124876a052a6b8bf6ef920d
michelepiccinno <michele.piccinno@gmail.com>
2023-08-22 16:30:20 +0200

    add header

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,31 @@
+<script>
+export default {}
+</script>
+
+<template>
+  <nav class="navbar bg-body-tertiary">
+    <div class="container">
+      <a class="navbar-brand" href="#">
+        <img src="../assets/img/dc-logo.png" alt="Bootstrap" width="30" height="24">
+      </a>
+      <ul class="nav nav-pills">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">CHARACTERS</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">COMICS</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">MOVIES</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">TV</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
